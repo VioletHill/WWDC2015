bd533e29583278a21bb9d429394fa116903f2e08
LazyBear <lazybear@liulishuo.com>
2015-04-25 00:55:37 +0800

    搭建PERSONAL

--- QiuFeng/QFPersonalViewController.swift
+++ QiuFeng/QFPersonalViewController.swift
@@ -8,8 +8,33 @@
 
 import UIKit
 
-class QFPersonalViewController: UIViewController {
+class QFPersonalViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     override func viewDidLoad() {
         self.view.backgroundColor = UIColor.appBlueColor()
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 2
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 22.0
+    }
+    
+    func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        if section == 0 {
+            return "INTRO"
+        }
+        else {
+            return "INTERESTS"
+        }
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        return UITableViewCell()
     }
 }
