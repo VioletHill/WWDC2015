7fc0dcc9e00a9c189c10ed6336e11ce199e10c98
LazyBear <lazybear@liulishuo.com>
2015-04-24 13:34:17 +0800

    首页起泡适配

--- QiuFeng/QFDashboardViewController.swift
+++ QiuFeng/QFDashboardViewController.swift
@@ -60,7 +60,55 @@ class QFDashboardViewController: UIViewController, UIScrollViewDelegate, UINavig
     
     // MARK: - Project Button
     
-    let projectButtonSize: CGFloat = 120
+    let projectButtonSize: CGFloat = {
+        if DeviceType.IS_IPHONE_4_OR_LESS {
+            return 120
+        }
+        else if DeviceType.IS_IPHONE_5 {
+            return 120
+        }
+        else if DeviceType.IS_IPHONE_6 {
+            return 140
+        }
+        else if DeviceType.IS_IPHONE_6P {
+            return 170
+        }
+        return 120
+    }()
+    
+    let projectButtonX: CGFloat = {
+        if DeviceType.IS_IPHONE_4_OR_LESS {
+            return 70
+        }
+        else if DeviceType.IS_IPHONE_5 {
+            return 70
+        }
+        else if DeviceType.IS_IPHONE_6 {
+            return 80
+        }
+        else if DeviceType.IS_IPHONE_6P {
+            return 80
+        }
+        
+       return 50
+    }()
+    
+    let projectY: CGFloat = {
+        if DeviceType.IS_IPHONE_4_OR_LESS {
+            return 110
+        }
+        else if DeviceType.IS_IPHONE_5 {
+            return 130
+        }
+        else if DeviceType.IS_IPHONE_6 {
+            return 180
+        }
+        else if DeviceType.IS_IPHONE_6P {
+            return 180
+        }
+        return 100
+    }()
+    
     lazy var projectButton: UIButton = {
         let project = UIButton.buttonWithType(UIButtonType.System) as! UIButton
         project.layer.cornerRadius = self.projectButtonSize / 2
@@ -74,7 +122,7 @@ class QFDashboardViewController: UIViewController, UIScrollViewDelegate, UINavig
     lazy var projectMoveAnimation: POPSpringAnimation = {
         let springAnimation = POPSpringAnimation(propertyNamed: kPOPLayerPositionY)
         springAnimation.fromValue = self.view.frame.size.height
-        springAnimation.toValue = 100
+        springAnimation.toValue = self.projectY
         springAnimation.springBounciness = 15
         springAnimation.springSpeed = 20
         springAnimation.dynamicsTension = 200
@@ -84,7 +132,56 @@ class QFDashboardViewController: UIViewController, UIScrollViewDelegate, UINavig
     
     // MARK: - Skill Button
     
-    let skillButtonSize: CGFloat = 140
+    let skillButtonSize: CGFloat = {
+        if DeviceType.IS_IPHONE_4_OR_LESS {
+            return 140
+        }
+        else if DeviceType.IS_IPHONE_5 {
+            return 140
+        }
+        else if DeviceType.IS_IPHONE_6 {
+            return 160
+        }
+        else if DeviceType.IS_IPHONE_6P {
+            return 200
+        }
+        
+        return 140
+    }()
+    
+    let skillButtonX: CGFloat = {
+        if DeviceType.IS_IPHONE_4_OR_LESS {
+            return 140
+        }
+        else if DeviceType.IS_IPHONE_5 {
+            return 140
+        }
+        else if DeviceType.IS_IPHONE_6 {
+            return 180
+        }
+        else if DeviceType.IS_IPHONE_6P {
+            return 185
+        }
+        return 150
+    }()
+    
+    let skillY: CGFloat = {
+        if DeviceType.IS_IPHONE_4_OR_LESS {
+            return 180
+        }
+        else if DeviceType.IS_IPHONE_5 {
+            return 200
+        }
+        else if DeviceType.IS_IPHONE_6 {
+            return 250
+        }
+        else if DeviceType.IS_IPHONE_6P {
+            return 260
+        }
+        
+        return 150
+    }()
+    
     lazy var skillButton: UIButton = {
         let skill = UIButton.buttonWithType(UIButtonType.System) as! UIButton
         skill.layer.cornerRadius = self.skillButtonSize / 2
@@ -98,7 +195,7 @@ class QFDashboardViewController: UIViewController, UIScrollViewDelegate, UINavig
     lazy var skillMoveAnimation: POPSpringAnimation = {
         let springAnimation = POPSpringAnimation(propertyNamed: kPOPLayerPositionY)
         springAnimation.fromValue = self.view.frame.size.height
-        springAnimation.toValue = 150
+        springAnimation.toValue = self.skillY
         springAnimation.springBounciness = 20
         springAnimation.springSpeed = 15
         springAnimation.dynamicsTension = 80
@@ -107,10 +204,57 @@ class QFDashboardViewController: UIViewController, UIScrollViewDelegate, UINavig
     
     //MARK: - Experience Button
     
-    let experienceSize: CGFloat = 100
+    let experienceButtonSize: CGFloat = {
+        if DeviceType.IS_IPHONE_4_OR_LESS {
+            return 100
+        }
+        else if DeviceType.IS_IPHONE_5 {
+            return 100
+        }
+        else if DeviceType.IS_IPHONE_6 {
+            return 120
+        }
+        else if DeviceType.IS_IPHONE_6P {
+            return 150
+        }
+        return 100
+    }()
+    
+    let experienceX: CGFloat = {
+        if DeviceType.IS_IPHONE_4_OR_LESS {
+            return 60
+        }
+        else if DeviceType.IS_IPHONE_5 {
+            return 60
+        }
+        else if DeviceType.IS_IPHONE_6 {
+            return 90
+        }
+        else if DeviceType.IS_IPHONE_6P {
+            return 95
+        }
+        return 60
+    }()
+    
+    let experienceY: CGFloat = {
+        if DeviceType.IS_IPHONE_4_OR_LESS {
+            return 290
+        }
+        else if DeviceType.IS_IPHONE_5 {
+            return 330
+        }
+        else if DeviceType.IS_IPHONE_6 {
+            return 390
+        }
+        else if DeviceType.IS_IPHONE_6P {
+            return 440
+        }
+        return 290
+    }()
+    
     lazy var experienceButton: UIButton = {
        let experience = UIButton.buttonWithType(UIButtonType.System) as! UIButton
-        experience.layer.cornerRadius = self.experienceSize / 2
+        experience.layer.cornerRadius = self.experienceButtonSize / 2
         experience.setTitle("Experience", forState: UIControlState.Normal)
         experience.backgroundColor = UIColor.appOrangeColor()
         experience.setTitleColor(UIColor.whiteColor(), forState: UIControlState.Normal)
@@ -121,7 +265,7 @@ class QFDashboardViewController: UIViewController, UIScrollViewDelegate, UINavig
     lazy var experienceMoveAnimation: POPSpringAnimation = {
         let springAnimation = POPSpringAnimation(propertyNamed: kPOPLayerPositionY)
         springAnimation.fromValue = self.view.frame.size.height
-        springAnimation.toValue = 300
+        springAnimation.toValue = self.experienceY
         springAnimation.springBounciness = 20
         springAnimation.springSpeed = 15
         springAnimation.dynamicsTension = 200
@@ -130,10 +274,58 @@ class QFDashboardViewController: UIViewController, UIScrollViewDelegate, UINavig
     
     //MARK: - Personal
     
-    let personalSize: CGFloat = 90
+    let personalButtonSize: CGFloat = {
+        if DeviceType.IS_IPHONE_4_OR_LESS {
+            return 90
+        }
+        else if DeviceType.IS_IPHONE_5 {
+            return 90
+        }
+        else if DeviceType.IS_IPHONE_6 {
+            return 110
+        }
+        else if DeviceType.IS_IPHONE_6P {
+            return 140
+        }
+        
+        return 90
+    }()
+    
+    let personalX: CGFloat = {
+        if DeviceType.IS_IPHONE_4_OR_LESS {
+            return 180
+        }
+        else if DeviceType.IS_IPHONE_5 {
+            return 180
+        }
+        else if DeviceType.IS_IPHONE_6 {
+            return 220
+        }
+        else if DeviceType.IS_IPHONE_6P {
+            return 225
+        }
+        return 180
+    }()
+    
+    var personalY: CGFloat = {
+        if DeviceType.IS_IPHONE_4_OR_LESS {
+            return 330
+        }
+        else if DeviceType.IS_IPHONE_5 {
+            return 380
+        }
+        else if DeviceType.IS_IPHONE_6 {
+            return 460
+        }
+        else if DeviceType.IS_IPHONE_6P {
+            return 520
+        }
+        return 320
+    }()
+    
     lazy var personalButton: UIButton = {
         let personal = UIButton.buttonWithType(UIButtonType.System) as! UIButton
-        personal.layer.cornerRadius = self.personalSize / 2
+        personal.layer.cornerRadius = self.personalButtonSize / 2
         personal.setTitle("Personal", forState: UIControlState.Normal)
         personal.backgroundColor = UIColor.appDarkGrayColor()
         personal.setTitleColor(UIColor.whiteColor(), forState: UIControlState.Normal)
@@ -144,7 +336,7 @@ class QFDashboardViewController: UIViewController, UIScrollViewDelegate, UINavig
     lazy var personalMoveAnimation: POPSpringAnimation = {
         let springAnimation = POPSpringAnimation(propertyNamed: kPOPLayerPositionY)
         springAnimation.fromValue = self.view.frame.size.height
-        springAnimation.toValue = 320
+        springAnimation.toValue = self.personalY
         springAnimation.springBounciness = 20
         springAnimation.springSpeed = 14
         springAnimation.dynamicsTension = 230
@@ -156,17 +348,17 @@ class QFDashboardViewController: UIViewController, UIScrollViewDelegate, UINavig
         self.experienceMoveAnimation.beginTime = CACurrentMediaTime() + 0.2
         self.personalMoveAnimation.beginTime = CACurrentMediaTime() + 0.4
         
-        self.projectButton.frame = CGRectMake(50, self.view.frame.size.height, self.projectButtonSize, self.projectButtonSize)
-        self.skillButton.frame = CGRectMake(150, self.view.frame.size.height, self.skillButtonSize, self.skillButtonSize)
-        self.experienceButton.frame = CGRectMake(40, self.view.frame.size.height, self.experienceSize, self.experienceSize)
-        self.personalButton.frame = CGRectMake(180, self.view.frame.size.height, self.personalSize, self.personalSize)
+        self.projectButton.frame = CGRectMake(self.projectButtonX, self.view.frame.size.height, self.projectButtonSize, self.projectButtonSize)
+        self.skillButton.frame = CGRectMake(self.skillButtonX, self.view.frame.size.height, self.skillButtonSize, self.skillButtonSize)
+        self.experienceButton.frame = CGRectMake(self.experienceX, self.view.frame.size.height, self.experienceButtonSize, self.experienceButtonSize)
+        self.personalButton.frame = CGRectMake(self.personalX, self.view.frame.size.height, self.personalButtonSize, self.personalButtonSize)
     }
     
     func showAnimationAtPage2() {
        
         
         if !self.page2View.subviews.contains(self.projectButton) {
-            print("show animation at page2")
+
             
             animationInit()
             
