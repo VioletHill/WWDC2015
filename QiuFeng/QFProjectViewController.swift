2fa68f9cb56c0b169710b8bb77c5eb1f234362e0
LazyBear <lazybear@liulishuo.com>
2015-04-24 20:43:03 +0800

    添加launch image 和 icon

--- QiuFeng/QFProjectViewController.swift
+++ QiuFeng/QFProjectViewController.swift
@@ -19,4 +19,21 @@ class QFProjectViewController: UIViewController {
     override func viewWillDisappear(animated: Bool) {
         super.viewWillDisappear(animated)
     }
+    
+    let baseUrl = "itms-apps://itunes.apple.com/app/id"
+    
+    @IBAction func tjfaIconPress(sender: UIButton) {
+        UIApplication.sharedApplication().openURL(NSURL(string: (baseUrl + "904654597"))!)
+    }
+    
+    @IBAction func snakeIconPress(sender: AnyObject) {
+        UIApplication.sharedApplication().openURL(NSURL(string: (baseUrl + "828815846"))!)
+    }
+    @IBAction func tingyuanIconPress(sender: AnyObject) {
+        let alertView = UIAlertController(title: "iPad Application", message: "This Application Only Support iPad", preferredStyle: .Alert)
+        let action =  UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: {(alert: UIAlertAction!) in self.dismissViewControllerAnimated(true, completion: nil)
+        })
+        alertView.addAction(action)
+        self.presentViewController(alertView, animated: true, completion: nil)
+    }
 }
